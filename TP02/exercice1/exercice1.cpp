175b2b8a468c5e79e3d5982f5e9aa005f73a647e
Thomas Venouil <venouilthomas123456@gmail.com>
2024-09-13 08:38:51 +0200

    Ajout des exos pour le TP2

---
+++ TP02/exercice1/exercice1.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+#include "EntierContraint.h"
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    // A Compléter
+    // Il faut tester operator int, operator << et operator >> sur la classe EntierContraint
+
+    return 0;
+}
+
